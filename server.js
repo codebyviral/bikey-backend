b11bab721c21a5fd2af612b90bc1d3ea3fc6dee5
Viral Vaghela <viralvaghela8577@gmail.com>
2024-12-09 22:26:35 +0530

    update - backend

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from "express";
 
 const app = express();
+const port = 4000;
 
-
-app.use("/",(req,res)=>{
+app.get("/",(req,res)=>{
     res.send(`Bikey. Wheels for your moments!`);
 })
+
+app.listen(port, () => {
+    console.log(`Bikey app listening on port ${port}`)
+  })
